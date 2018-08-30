479042df2a60675c75df11d8c75fd2ea74c831b3
stitchy1 <595430577@qq.com>
2018-08-30 14:13:33 -0400

    Signed-off-by: stitchy1 <595430577@qq.com>

--- pretreatment/illustrateCSP.m
+++ pretreatment/illustrateCSP.m
@@ -32,27 +32,65 @@ projection_j_12 = getprojection(EEGSignals_j_12, CSPMatrix_j_12, nbFilterPairs);
 [m,n]=size(projection_j_12);
 t=1:n;
 figure
-v(1)=var(A{1,p}(:,1)',1,2)-350;
+v(1)=var(A{1,p}(:,1)',1,2);
 v(2)=var(A{2,k}(:,1)',1,2);
 v(3)=var(projection_j_12(p,:),1,2);
 v(4)=var(projection_j_12(b+k,:),1,2);
 
 % raw
-subplot(2,1,1)
+subplot(2,2,1)
 plot(t,A{1,p}(:,1)','b','linewidth',0.75);hold on;
 plot(t,A{2,k}(:,1)'-120,'r','linewidth',0.75);hold off;
 axis([0 1505 -inf 150]);
 title('Raw Data');
 xlabel('t/(1/512)s');
 ylabel('Amplitude');
-legend(['Air-conditioner: ' num2str(v(1))],['Light: ' num2str(v(2))]);
+legend(['var:' num2str(v(1))],['var:' num2str(v(2))]);
 
 % projection
-subplot(2,1,2)
+subplot(2,2,3)
 plot(t,projection_j_12(p,:),'b','linewidth',0.75);hold on;
 plot(t,projection_j_12(b+k,:),'r','linewidth',0.75);hold off;
 axis([0 1505 -inf inf]);
 title('Projected Data');
 xlabel('t/(1/512)s');
 ylabel('Amplitude');
-legend(['Air-conditioner: ' num2str(v(3))],['Light: ' num2str(v(4))]);
+legend(['var:' num2str(v(3))],['var:' num2str(v(4))]);
+
+%% plot spectrum
+Fs=512;                 %È¡ÑùÆµÂÊ
+L=1506;                 %ÐòÁÐ³¤¶È
+f = Fs*(0:(L/2))/L;
+Y=fft(A{1,p}(:,1)');  %¸µÀïÒ¶±ä»»
+P2 = abs(Y/L);          %Ë«±ßÆ×
+P1 = P2(1:L/2+1);       
+P1(2:end-1) = 2*P1(2:end-1);%µ¥±ßÆ×
+subplot(2,2,2);
+plot(f,P1,'b','linewidth',0.75);hold on;
+Y=fft(A{1,p}(:,1)');  %¸µÀïÒ¶±ä»»
+P2 = abs(Y/L);          %Ë«±ßÆ×
+P1 = P2(1:L/2+1);       
+P1(2:end-1) = 2*P1(2:end-1);%µ¥±ßÆ×
+plot(f,P1-20,'r','linewidth',0.75);hold off;
+axis([0 128 -inf 30]);
+title(['Single-Sided Spectrum of Raw Data(',num2str(a),',',num2str(b),')']);
+xlabel('f/Hz');
+ylabel('Amplitude');
+legend('P1','P2-20');
+
+Y=fft(projection_j_12(p,:));  %¸µÀïÒ¶±ä»»
+P2 = abs(Y/L);          %Ë«±ßÆ×
+P1 = P2(1:L/2+1);       
+P1(2:end-1) = 2*P1(2:end-1);%µ¥±ßÆ×
+subplot(2,2,4);
+plot(f,P1,'b','linewidth',0.75);hold on;
+Y=fft(projection_j_12(b+k,:));  %¸µÀïÒ¶±ä»»
+P2 = abs(Y/L);          %Ë«±ßÆ×
+P1 = P2(1:L/2+1);       
+P1(2:end-1) = 2*P1(2:end-1);%µ¥±ßÆ×
+plot(f,P1-10,'r','linewidth',0.75);hold off;
+axis([0 128 -inf inf]);
+title(['Single-Sided Spectrum of Raw Data(',num2str(a),',',num2str(b),')']);
+xlabel('f/Hz');
+ylabel('Amplitude');
+legend('P1','P2-10');
